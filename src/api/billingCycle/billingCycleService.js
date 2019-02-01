f4ae66070292b753a76a77d65989951be418519d
henriquegomes6 <henriquegomes6@hotmail.com>
2019-01-31 22:53:21 -0200

    add route count

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ import BillingCycle from './billingCycle';
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+            res.status(500).json({ errors: [error] });
+        }
+        
+        res.json({ value });
+    })
+})
+
 export default BillingCycle;
